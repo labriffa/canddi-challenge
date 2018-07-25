c6e65ff099d074e6beae7706f06222ede6cffea2
lewisbriffa <lewis.a.briffa@gmail.com>
2018-07-25 21:18:42 +0100

    Telephone extraction works generally ok, but wont work on sites like Sputnik Digital.

--- app.js
+++ app.js
@@ -15,22 +15,36 @@ const EmailLib = require('./utilities/EmailLib');
 
 const options = {
 	method: 'GET',
-	uri: 'https://www.canddi.com/privacy/'
+	uri: 'http://www.thomaswillmax.co.uk/contact-us/'
 };
 
 request(options)
 	.then((response) => {
+		const $ = cheerio.load(response);
 
-		// run initial knwl parser functions
-		knwlInstance.init(response);		
+		// remove conflicting tags
+		$('head, script').remove();
+
+		// use lookaround based regex to remove the spaces between digits
+		const text = $.text().replace(/(?<=\d) +(?=\d)/g, '');
+
+		knwlInstance.init(text);		
 
 		// get emails
 		const emails = knwlInstance.get('emails');
+		const uniqEmails = [...new Set(emails.map(({ address }) => address))];
 
 		// get international phone numbers
 		const internationalPhones = knwlInstance.get('internationalPhones');
-		const uniqInternationalPhones = [...(new Set(internationalPhones.map(({ number }) => number)))];
+		const uniqInternationalPhonesSet = new Set(internationalPhones.map(({ number }) => number));
+
+		// try interpreting non-internationalized numbers
+		const phones = knwlInstance.get('phones');
+		const uniqPhonesSet = new Set(phones.map(({ phone }) => phone));
+
+		// combine both international and non-international numbers
+		const uniqPhones = [...new Set([...uniqInternationalPhonesSet, ...uniqPhonesSet])];
 	})
 	.catch((error) => {
-		console.log(error);
+		console.log('Unable to parse');
 	});
