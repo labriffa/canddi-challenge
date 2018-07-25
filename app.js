b61ee0ecf5e2ac9c750daa1a4dc9319a996bc338
lewisbriffa <lewis.a.briffa@gmail.com>
2018-07-25 19:19:51 +0100

    Scrape email addresses from a single page

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 'use strict';
 
 // Dependencies -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-const request = require('request');
+const request = require('request-promise');
 const cheerio = require('cheerio');
+const Knwl = require('knwl.js');
+
+// Configure Knwl
+const knwlInstance = new Knwl('english');
 
 // Utilities -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const EmailLib = require('./utilities/EmailLib');
 
+const options = {
+	method: 'GET',
+	uri: 'https://www.canddi.com/privacy/'
+};
+
+request(options)
+	.then((response) => {
+		const $ = cheerio.load(response);
+
+		// run initial knwl parser functions
+		knwlInstance.init(response);
+
+		const emails = knwlInstance.get('emails');
+	})
+	.catch((error) => {
+		console.log(error);
+	});
